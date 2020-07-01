8cfcefc5dd2de2a0a80253d856f17c9cfa7714fe
Tristan9497 <tristan@5schwoerer.de>
2020-07-01 23:40:45 +0200

    +sepperate threading.timer +shifting and rotation of the pieces

--- Tetris.py
+++ Tetris.py
@@ -1,89 +1,120 @@
 from p5 import *
+from array import *
 import numpy
-height=800
-width=1600
-xstart=600
-ystart=0
+import random
+import time,threading
+
+height = 800
+width = 1600
+xstart = 600
+ystart = 0
 zellsy = 20
 zellsx = 10
-zellsize=height/zellsy
-play_width=zellsize*zellsx
-play_height=zellsize*zellsy
+zellsize = height/zellsy
+play_width = zellsize*zellsx
+play_height = zellsize*zellsy
+
+startpointx=5
+startpointy=0
 
 Types=['I','O','S','Z','T','J','L']
-Shapes=[['0000',
-         '0000',
-         'XXXX',
-         '0000'],
-        ['0000',
-         '0XX0',
-         '0XX0',
-         '0000'],
-        ['0XX',
-         'XX0',
-         '000'],
-        ['XX0',
-         '0XX',
-         '000'],
-        ['0X0',
-         'XXX',
-         '000'],
-        ['X00',
-         'XXX',
-         '000'],
-        ['00X',
-         'XXX',
-         '000']]
+#Coordinates of all Pieces relativ to center point top left corner for square piece
+Shapes=[[[-2,0],[-1,0],[0,0],[1,0]],
+        [[0,0],[1,0],[0,1],[1,1]],
+        [[0,0],[1,0],[-1,1],[0,1]],
+        [[-1,0],[0,0],[0,1],[1,1]],
+        [[-1,-1],[0,-1],[1,-1],[0,0]],
+        [[-1,0],[0,0],[1,0],[1,1]],
+        [[-1,0],[0,0],[1,0],[-1,1]]]
 
-
+rotationmatrix=[[0,-1],[1,0]]
+trigger=1
 def setup():
-    global Board1
-
+    global Board1,Block1
     size(width,height)
-
     Board1=Board(width,height)
+    Block1=Stone()
+    ShiftTimer()
+
+
+
 
 def draw():
     background(0)
     Board1.display()
+    Board1.drawblocks()
+    Block1.draw()
+
+
+def ShiftTimer():
+    threading.Timer(0.1,shift_down).start()
+
+
+def shift_down():
+    try:
+        Block1.position[1] += 1
+    except AttributeError:
+        None
+    threading.Timer(0.1, ShiftTimer).start()
+
+def key_pressed():
+    if key=='UP':
+        Block1.rotation()
+    if key=='LEFT':
+        Block1.move('Left')
+    if key=='RIGHT':
+        Block1.move('Right')
+
+
 
 class Stone(object):
-    def __init__(self,Type):
-        self.Type=Type
-        self.shape=Shapes[Types.index(Type)]
+    def __init__(self):
+        self.position=[startpointx,startpointy]
+        self.Type = random.choice(Types)
+        self.shape=Shapes[Types.index(self.Type)]
+
     def rotation(self):
-        self.shape=rotateblock(self)
 
+        if self.Type == 'O':
+            self.shape=self.shape
+        else:
+            for i in self.shape:
+                x = i[0] * rotationmatrix[0][0] + i[1] * rotationmatrix[1][0]
+                print(i[0])
+                y = i[0] * rotationmatrix[0][1] + i[1] * rotationmatrix[1][1]
+                i[0]=x
+                i[1]=y
 
-def rotateblock(Block):
-    if Block.Type=='O':
-        return Block.shape
-    elif Block.Type=='I':
-        if Block.shape[0][2]=='X':
-            Block.shape=['0000',
-                         '0000',
-                         'XXXX',
-                         '0000']
-        elif Block.shape[2][0]=='X':
-            Block.shape=['00X0',
-                         '00X0',
-                         '00X0',
-                         '00X0']
-    else:
-        #TODO
-        return 3
+    def move(self,Direction):
+        if Direction=='Right':
+            if not (max(x[0] for x in self.shape) + int(self.position[0])) > 8:
+                self.position[0]+=1
+        if Direction == 'Left':
+            if not (min(x[0] for x in self.shape) + int(self.position[0])) < 1:
+                self.position[0]-=1
 
-
-
-
+    def draw(self):
+        for i in self.shape:
+            fill(255)
+            rect((xstart+(self.position[0]+i[0])*zellsize,ystart+(self.position[1]+i[1])*zellsize),zellsize,zellsize)
 
 class Board(object):
+
     def __init__(self,width,height):
-        self.width=width
-        self.height=height
+        self.occupied = np.zeros((zellsy,zellsx), dtype=int)
+        self.width = width
+        self.height = height
 
     def display(self):
         drawgrid()
+    def drawblocks(self):
+        for i in range(0, zellsy - 1):
+            for j in range(0, zellsx - 1):
+                if self.occupied[i][j] != 0:
+                    print(self.occupied[i][j])
+                    fill(self.occupied[i][j])
+                    rect((xstart+j*zellsize,ystart+i*zellsize), zellsize, zellsize)
+
 
 def drawgrid():
     line_color=255
@@ -94,4 +125,4 @@ def drawgrid():
         line((xstart,ystart+ j*zellsize), (xstart+play_width, ystart+j*zellsize))
 
 if __name__ == '__main__':
-    run()
+    run(frame_rate=10)
