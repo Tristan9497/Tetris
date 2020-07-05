a9c4f685e5682c657169e76e710afe714228f3d7
Tristan9497 <tristan@5schwoerer.de>
2020-07-05 18:20:44 +0200

    Fixed Moving issue

--- Tetris.py
+++ Tetris.py
@@ -30,11 +30,15 @@ Shapes=[[[-2,0],[-1,0],[0,0],[1,0]],
         [[-1,0],[0,0],[1,0],[-1,1]]]
 
 rotationmatrix=[[0,-1],[1,0]]
+
+Colors=[[0,255,255],[255,255,0],[0,255,0],[255,0,0],[128,0,128],[0,0,255],[255,165,0]]
+
 def setup():
-    global Board1,Block1,stopFlag
+    global Board1, CurrentBlock, stopFlag, PieceTrigger
+    PieceTrigger = False
     size(width,height)
     Board1=Board(width,height)
-    Block1=Stone()
+    CurrentBlock=Stone()
 
     stopFlag = Event()
     thread=MyThread(stopFlag)
@@ -44,9 +48,10 @@ def draw():
     background(0)
     Board1.display()
     Board1.drawblocks()
-    Block1.draw()
+    CurrentBlock.draw()
 
 
+#Timer Thread for Downwards movement
 class MyThread(Thread):
     def __init__(self, event):
         Thread.__init__(self)
@@ -55,71 +60,131 @@ class MyThread(Thread):
 
     def run(self):
         while not self.stopped.wait(0.5):
-            try:
-                Block1.move('Down')
-            except:
-                print('Test')
-
+            if (PieceTrigger):
+                CurrentBlock.__init__()
+            else:
+                CurrentBlock.move('Down')
 
 #Keyboard events
 def key_pressed(event):
     if key=='UP':
-        Block1.rotation()
+        CurrentBlock.rotation()
     if key=='LEFT':
-        Block1.move('Left')
+        CurrentBlock.move('Left')
     if key=='RIGHT':
-        Block1.move('Right')
+        CurrentBlock.move('Right')
+    if key=='DOWN':
+        CurrentBlock.move('Down')
 
 class Stone(object):
     def __init__(self):
+        global PieceTrigger,stopFlag
         self.position=[startpointx,startpointy]
         self.Type = random.choice(Types)
         self.shape=Shapes[Types.index(self.Type)]
+        self.color=Colors[Types.index(self.Type)]
+        PieceTrigger=False
+
 
     def rotation(self):
-
         if self.Type == 'O':
             self.shape=self.shape
         else:
             for i in self.shape:
                 x = i[0] * rotationmatrix[0][0] + i[1] * rotationmatrix[1][0]
-                print(i[0])
                 y = i[0] * rotationmatrix[0][1] + i[1] * rotationmatrix[1][1]
                 i[0]=x
                 i[1]=y
 
+
     def move(self,Direction):
+        global PieceTrigger
+        buffershape=self.shape
+        bufferposition=self.position
+
+
         if Direction=='Right':
-            if not (max(x[0] for x in self.shape) + int(self.position[0])) > 8:
-                self.position[0]+=1
-        if Direction == 'Left':
-            if not (min(x[0] for x in self.shape) + int(self.position[0])) < 1:
-                self.position[0]-=1
+            # check pieces
+            if max(x[0] for x in buffershape) + bufferposition[0] < 9:
+                bufferposition[0] += 1
+                if checkOccupied(buffershape, bufferposition):
+                    bufferposition[0] -= 1
+                    updatecoordinates(self.shape, self.position)
+            #check boarders
+            elif max(x[0] for x in buffershape)+bufferposition[0]>=9:
+                updatecoordinates(self.shape,self.position)
+            else:
+                updatecoordinates(buffershape, bufferposition)
+
+        elif Direction == 'Left':
+            #check pieces
+            if min(x[0] for x in buffershape) + bufferposition[0] >0:
+                bufferposition[0] -= 1
+                if checkOccupied(buffershape, bufferposition):
+                    bufferposition[0] += 1
+                    updatecoordinates(self.shape, self.position)
+            # check boarders
+            elif min(x[0] for x in buffershape) + bufferposition[0]<=0:
+                updatecoordinates(self.shape, self.position)
+            else:
+                updatecoordinates(buffershape, bufferposition)
+
         if Direction=='Down':
-            if not int(self.position[1])-(min(y[1] for y in self.shape))> (zellsy-2):
-                self.position[1]+=1
+            if max(y[1] for y in buffershape) + bufferposition[1] >= 19:
+                addPieceToOccupied(self)
+            # check pieces
+            elif max(y[1] for y in buffershape) + bufferposition[1] < 19:
+                bufferposition[1] += 1
+                if checkOccupied(buffershape, bufferposition):
+                    bufferposition[1] -= 1
+                    # add currentpiece to occupied and spawn new piece
+                    addPieceToOccupied(self)
+            else:
+                # Set new Position
+                updatecoordinates(buffershape, bufferposition)
 
     def draw(self):
         for i in self.shape:
-            fill(255)
+            fill(self.color[0],self.color[1],self.color[2])
             rect((xstart+(self.position[0]+i[0])*zellsize,ystart+(self.position[1]+i[1])*zellsize),zellsize,zellsize)
+
+
+def updatecoordinates(shape, position):
+    CurrentBlock.position = position
+    CurrentBlock.shape = shape
+
+def addPieceToOccupied(Block):
+    global PieceTrigger
+    for i in Block.shape:
+        for j in range(0, len(Block.color)):
+            Board1.occupied[(Block.position[1] + i[1]), (Block.position[0] + i[0]), (j)] = Block.color[j]
+    PieceTrigger = True
+
+def checkOccupied(shape,position):
+    Adder=0
+    for x in shape:
+        Adder+= sum(Board1.occupied[x[1] + position[1], x[0] + position[0]])
+    if Adder>0:
+        return True
+    else:
+        return False
 
 class Board(object):
 
     def __init__(self,width,height):
-        self.occupied = np.zeros((zellsy,zellsx), dtype=int)
+        self.occupied = np.zeros((zellsy,zellsx,3), dtype=int)
         self.width = width
         self.height = height
 
     def display(self):
         drawgrid()
+
     def drawblocks(self):
-        for i in range(0, zellsy - 1):
-            for j in range(0, zellsx - 1):
-                if self.occupied[i][j] != 0:
-                    print(self.occupied[i][j])
-                    fill(self.occupied[i][j])
+        for i in range(0, zellsy):
+            for j in range(0, zellsx):
+                    fill(self.occupied[i,j,0],self.occupied[i,j,1],self.occupied[i,j,2])
                     rect((xstart+j*zellsize,ystart+i*zellsize), zellsize, zellsize)
+
 
 def drawgrid():
     line_color=255
@@ -130,4 +195,4 @@ def drawgrid():
         line((xstart,ystart+ j*zellsize), (xstart+play_width, ystart+j*zellsize))
 
 if __name__ == '__main__':
-    run()
+    run(frame_rate=30)
